bef1140a3ca3e504d79deb6f379fc9aef20f033f
warrior-zhao <zhaoyong408@gmail.com>
2014-12-29 15:59:29 +0800

    c++ solution for max area problem

--- max_area.cpp
+++ max_area.cpp
@@ -1,12 +1,37 @@
 #include <vector>
 #include <stdio.h>
+#include <limits.h>
 
 using std::vector;
+using std::min;
+using std::max;
 
 class Solution {
 public:
     int maxArea(vector<int> &height) {
-        return 0;
+        size_t array_size = height.size();
+        if(array_size <= 1)
+        {
+            return 0;
+        }
+
+        int max_area = 0;
+        int left = 0;
+        int right = (int)(array_size - 1);
+        while(left < right)
+        {
+            max_area = max(max_area, (right-left)*min(height[left], height[right]));
+            if(height[left] > height[right])
+            {
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+        
+        return max_area;
     }
 };
 
