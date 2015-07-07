78321f4be826f0b2872cce5ed4760c23b09af3eb
warrior_zhao <zhaoyong408@gmail.com>
2015-07-07 17:33:28 +0800

    initial commit for regular expression problem, python edition

---
+++ regular_expression.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param {string} s
+    # @param {string} p
+    # @return {boolean}
+    def isMatch(self, s, p):
+        pass
+
+
+import sys
+
+s = sys.argv[1]
+p = sys.argv[2]
+solution = Solution()
+matched = solution.isMatch(s, p)
+print matched
