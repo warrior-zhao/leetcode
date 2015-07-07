9f4ccd098804b2ec3b1c997365ebda7a5a1c50aa
zhaoyong <zhaoyong@proplussolution.com>
2015-07-07 17:04:29 +0800

    c++ solution for regular expression problem

---
+++ regular_expression.cpp
@@ -0,0 +1,61 @@
+#include <string>
+#include <stdio.h>
+
+using std::string;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if(s == p)
+        {
+            return true;
+        }
+        if(p.empty())
+        {
+            return false;
+        }
+
+        bool char_match = false;
+        if(!s.empty() && (s.at(0) == p.at(0) || '.' == p.at(0)))
+        {
+            char_match = true;
+        }
+
+        bool expression_matched = false;
+        if(p.size() >= 2 && '*' == p.at(1))
+        {
+            expression_matched = isMatch(s, p.substr(2));
+            if(char_match && !expression_matched)
+            {
+                expression_matched = isMatch(s.substr(1), p);
+            } 
+        }
+        else if(char_match)
+        {
+            expression_matched = isMatch(s.substr(1), p.substr(1));
+        }
+
+        return expression_matched;
+    }
+
+};
+
+int main(int argc, char* argv[])
+{
+    if(argc <= 2)
+    {
+        return 0;
+    }
+
+    Solution solution;
+    bool is_matched = solution.isMatch(argv[1], argv[2]);
+    if(is_matched)
+    {
+        printf("%s %s matched! \n", argv[1], argv[2]);
+    }
+    else
+    {
+        printf("%s %s NOT matched!\n", argv[1], argv[2]);
+    }
+    return 0;
+}
