263eba3a2ef988d48468f0099b002c0e1f0a4e63
warrior_zhao <zhaoyong408@gmail.com>
2015-07-08 15:00:17 +0800

    python implementation, but failed the online judgement...

--- regular_expression.py
+++ regular_expression.py
@@ -3,8 +3,23 @@ class Solution:
     # @param {string} p
     # @return {boolean}
     def isMatch(self, s, p):
-        pass
+        if s == p:
+            return True
+        if len(p) == 0:
+            return False
+            
+        char_matched = False
+        if len(s) > 0 and (s[0] == p[0] or p[0] == '.'):
+            char_matched = True
 
+        expression_matched = False
+        if len(p) >= 2 and p[1] == '*':
+            expression_matched = self.isMatch(s, p[2:])
+            if char_matched and not expression_matched:
+                expression_matched = self.isMatch(s[1:], p)
+        elif char_matched:
+            expression_matched = self.isMatch(s[1:], p[1:])
+        return expression_matched
 
 import sys
 
